82052127a96d9f55f8f3cea953c5edb151ba6e1b
Uriel Kosayev <36763863+MalFuzzer@users.noreply.github.com>
2021-08-16 09:40:38 +0300

    Create login_log_collector.sh

---
+++ login_log_collector.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+log_file="out.log"
+
+echo "Last loggedin users:" > $log_file
+last | head -n 2 >> $log_file
+echo "Cross-reference data:" >> $log_file
+cat /var/log/auth.log | grep ssh | tail -n 2 >> $log_file
+echo "" >> $log_file
+echo "IP address:" >> $log_file && ifconfig eth0 | grep -v inet6 | grep inet >> $log_file
