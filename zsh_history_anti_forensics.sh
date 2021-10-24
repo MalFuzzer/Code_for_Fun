e4cf517f7ded3ba8ab0cf09367ea652927be7476
Uriel Kosayev <36763863+MalFuzzer@users.noreply.github.com>
2021-10-25 00:01:36 +0300

    Update zsh_history_anti_forensics.sh

--- zsh_history_anti_forensics.sh
+++ zsh_history_anti_forensics.sh
@@ -1,1 +1,1 @@
-function  youHaveBeenHacked { local HISTSIZE=0;}; ifconfig &> /dev/null;  echo "" > ~/.bash_history; echo "" > ~/.zsh_history;  youHaveBeenHacked
+function  hist_rem { local HISTSIZE=0;}; ifconfig &> /dev/null;  echo "" > ~/.bash_history; echo "" > ~/.zsh_history;  hist_rem
